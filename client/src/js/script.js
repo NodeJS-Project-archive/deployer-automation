8083994520da17ad87445e9cc4823fd0deeb47db
Spanadana Bola <spbola@igategegdc.com>
2017-07-17 20:12:38 +0530

    deployer automation implemented

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -8,28 +8,6 @@ app.factory('socketFactory', ['$rootScope', function ($rootScope) {
         emit: function (eventName, data) {
             socket.emit(eventName, data);
         }
-        // on: function (eventName, callback) {
-        //     function wrapper() {
-        //         var args = arguments;
-        //         $rootScope.$apply(function () {
-        //             callback.apply(socket);
-        //         });
-        //     }
-        //     socket.on(eventName, wrapper);
-        //     return function () {
-        //         socket.removeListener(eventName, wrapper);
-        //     };
-        // },
-        // emit: function (eventName, data, callback) {
-        //     socket.emit(eventName, data, function () {
-        //         var args = arguments;
-        //         $rootScope.$apply(function () {
-        //             if (callback) {
-        //                 callback.apply(socket);
-        //             }
-        //         });
-        //     });
-        // }
     };
 }]);
 app.controller('myCtrl', function ($scope, socketFactory) {
@@ -48,21 +26,7 @@ app.controller('myCtrl', function ($scope, socketFactory) {
     }
 
     $scope.getResponse = function () {
-       // $scope.data = [];
         $scope.data.push($scope.responseData);
     }
-    // $scope.res = false;
-    // $scope.err = false;
-    // $http({
-    //     method: "GET",
-    //     url: "/childProcess"
-    // }).then(function mySuccess(response) {
-    //     $scope.res = true;
-    //     $scope.err = false;
-    //     $scope.data = response.data;
-    // }, function myError(err) {
-    //     $scope.res = false;
-    //     $scope.err = true;
-    //     $scope.error = err;
-    // });
+ 
 });
